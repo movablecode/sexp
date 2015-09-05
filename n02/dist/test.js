ab7370ebde7d05593600641bff7597703d5695ff
Lee Sangmin <leesangmin@172.20.nate.com>
2015-09-05 17:58:58 +0900

    add TEST

---
+++ n02/dist/test.js
@@ -0,0 +1,8 @@
+//  t01.jsx
+"use strict";
+
+var print = console.log;
+var chai = require("chai");
+
+var x = 10;
+print('let x');
---
+++ n02/src/test/t01.jsx
@@ -0,0 +1,6 @@
+//  t01.jsx
+let print = console.log;
+let chai = require("chai");
+
+let x = 10;
+print('let x');
