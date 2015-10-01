8c61b4a829761289d53520bd0f4c438a7fc5216c
Sangmin <sangmin.lna@gmail.com>
2015-10-01 18:32:50 +0900

    refine

--- elec01/distpack.bat
+++ elec01/distpack.bat
@@ -1,1 +1,1 @@
-electron-packager ./ Rage --platform=win32 --arch=ia32 --version=0.33.3 --ignore="node_modules" --ignore="distpack" --out="distpack" --cache="$HOME/.electron" --asar --icon="distpack/ico1.ico" --overwrite
+electron-packager ./ Rage --platform=win32 --arch=ia32 --version=0.33.3 --ignore="node_modules" --ignore="distpack" --out="distpack" --cache="$HOME/.electron" --asar --icon="distpack/ico1.ico"
--- elec01/index.js
+++ elec01/index.js
@@ -22,7 +22,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1280, height: 480});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
