5653f9b27e827c5e0af9f8310682def67b8c46f4
Sangmin Lee <smlee@vultr.guest>
2015-11-06 14:34:25 +0000

    add gulp

--- m03/gulpfile.js
+++ m03/gulpfile.js
@@ -3,8 +3,22 @@ var gulp    = require ('gulp');
 var babel   = require ('gulp-babel');
 var concat  = require ('gulp-concat');
 var uglify  = require ('gulp-uglify');
-var mocha   = require ('gulp-mocha');
+//var mocha   = require ('gulp-mocha');
 
 var print   = console.log
 var src_lib = 'src/lib/*.jsx';
-var src_test = 'src/routes/*.jsx';
+var src_routes = 'src/routes/*.jsx';
+
+//  routes 컴파일, 빌드.
+gulp.task('build_routes', function () {
+  return gulp.src(src_routes)
+    .pipe(babel())
+    .pipe(gulp.dest('routes'));
+});
+
+//  파일 변경 감시
+gulp.task('watch', function () {
+  gulp.watch(src_routes,['build_routes'])
+});
+
+gulp.task('default', ['build_routes','watch']);
